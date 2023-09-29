15eb02fc3a880df0df2ca9ac2463bf5b946b60a7
TimbirICT <tmiddlebrooks99@gmail.com>
2023-09-29 17:33:23 -0500

    added selection boxes for character types and length of password

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()_+";
+var numbers = "0123456789";
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,8 +20,44 @@ function writePassword() {
 
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+    var length = (document.getElementById("length").value);
+    var lowercase = document.getElementById("lowercase").checked;
+    var uppercase = document.getElementById("uppercase").checked;
+    var numbers = document.getElementById("numbers").checked;
+    var special = document.getElementById("special").checked;
 
 
-generatePassword();
+if(!special && !lowercase && !uppercase && !numbers) {
+    windows.alert("You must choose at least one character type");
+    
+
+}
+    
+}
+
+/*
+document.getElementById("Btn").addEventListener("click", writePassword);{
+var length = (document.getElementById("length").value);
+var lowercase = document.getElementById("lowercase").checked;
+var uppercase = document.getElementById("uppercase").checked;
+var numbers = document.getElementById("numbers").checked;
+var special = document.getElementById("special").checked;
+
+
+if(!special && !lowercase && !uppercase && !numbers) {
+    alert("You must choose at least one character type");
+    
+
+}
+}
+*/
+
+writePassword();
+
+
+
+
